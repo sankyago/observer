bb29b78ae056224446eda7f2b810c6d29577d258
Alexander Serdyuk <Sanyok2000xx@gmail.com>
2026-04-12 19:27:11 +0300

    feat: wire router and providers

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
+import FlowsListPage from './pages/FlowsListPage';
+import FlowEditorPage from './pages/FlowEditorPage';
 
 export default function App() {
   return (
     <ConfigProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>Loading...</div>} />
-          <Route path="/flows/:id" element={<div>Loading...</div>} />
+          <Route path="/" element={<FlowsListPage />} />
+          <Route path="/flows/:id" element={<FlowEditorPage />} />
         </Routes>
       </BrowserRouter>
     </ConfigProvider>
