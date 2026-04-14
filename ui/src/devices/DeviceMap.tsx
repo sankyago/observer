eff1a895a1b715bfbdd5f0f8d55b4e60ba4f3c22
Alexander Serdyuk <Sanyok2000xx@gmail.com>
2026-04-14 23:25:23 +0300

    fix(ui): drop unused React import in DeviceMap

--- ui/src/devices/DeviceMap.tsx
+++ ui/src/devices/DeviceMap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Polyline, Popup, useMap } from 'react-leaflet';
 import type { LatLngExpression } from 'leaflet';
 import L from 'leaflet';
